05c5aabb1c59e88abec1b4b9da04308e87066abf
soushi <sosimds@gmail.com>
2021-11-09 12:01:54 +0900

    renamer.pyに、ディレクトリを指定する機能と、正規表現でフィルターをかける機能を追加。

--- renamer.py
+++ renamer.py
@@ -4,26 +4,40 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-def rename(files, numonly=False):
-    logger.debug('リネームを開始します')
+def rename(files, numonly=False, match=None):
+    logger.info('リネームを開始します')
     files = [[os.path.abspath(i), int(re.search(r'.+?-([0-9]+?)\.txt', os.path.basename(i)).group(1))] for i in files]
+    if match:
+        files = [i for i in files if re.search(match, os.path.basename(i[0]))]
     no_length = len(str(max(files, key=lambda i:i[1])[1]))
 
+    logger.info('リネーム中 : {} ファイル'.format(len(files)))
     for file, no in files:
         newname = str(no).rjust(no_length, '0')+'.txt'
         if not numonly:
             newname = re.search(r'(.+?-)[0-9]+?\.txt', os.path.basename(file)).group(1) + newname
         logger.debug('rename : {} >>> {}'.format(os.path.basename(file), newname))
         os.rename(file, os.path.dirname(file)+os.sep+newname)
-    logger.debug('リネームが完了しました')
+    logger.info('リネームが完了しました')
+
+def rename_dir(dirname, numonly=False, match=None):
+    dirabs = os.path.abspath(dirname)
+    logger.info('ディレクトリからファイルを選択します : {}'.format(dirabs))
+
+    files = [dirabs + os.sep + i for i in os.listdir(dirabs)]
+    logger.debug('選択したファイル : {}'.format('\n'.join(files)))
+    rename(files, numonly=numonly, match=match)
+
 
 if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('input', nargs='*', help='リネームするファイル')
+    parser.add_argument('--input', '-i', nargs='*', help='リネームするファイル')
+    parser.add_argument('--dir', '-d', type=str, help='リネームするファイルが入ったディレクトリ')
+    parser.add_argument('--match', '-m', type=str, help='ファイルを正規表現でフィルタリングします')
     parser.add_argument('--numonly', '-n', action='store_true', help='数字のみのファイル名にする')
-    parser.add_argument('--debug', '-d', action='store_true', help='ログを表示する')
+    parser.add_argument('--debug', '-de', action='store_true', help='ログを表示する')
     args = parser.parse_args()
 
     LOGLEVEL = logging.WARNING
@@ -35,4 +49,9 @@ if __name__ == '__main__':
     streamHandler.setFormatter(logging.Formatter('%(levelname)s - %(name)s - %(message)s'))
     logger.addHandler(streamHandler)
 
-    rename(args.input, args.numonly)
+    if args.input:
+        rename(args.input, args.numonly, match=args.match)
+    elif args.dir:
+        rename_dir(args.dir, args.numonly, match=args.match)
+    else:
+        print('ファイルまたはディレクトリが指定されていません')
